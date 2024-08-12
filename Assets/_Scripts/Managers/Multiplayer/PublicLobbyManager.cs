7b12ea67382182e14df8e4c8e1c26654c572837c
V.431_ <waelabdelal7@gmail.com>
2024-08-12 17:00:42 +0300

    Fixes

--- Assets/_Scripts/Managers/Multiplayer/PublicLobbyManager.cs
+++ Assets/_Scripts/Managers/Multiplayer/PublicLobbyManager.cs
@@ -15,7 +15,7 @@ public class PublicLobbyManager : NetworkBehaviour, INetworkRunnerCallbacks
     private static LobbyHubManager _hubManager;
 
     private const int MAX_PLAYERS = 6;
-    private const int LOBBY_TIMER_START = 3;
+    private const int LOBBY_TIMER_START = 10;
     [Networked] public bool net_IsSpawned { get; set; }
 
     private SceneRef _gameScene;
